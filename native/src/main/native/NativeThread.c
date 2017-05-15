6c723c3735e74505a2d53307af18d3b42aa31969
Han Qiao <qh812@imperial.ac.uk>
2017-05-15 15:45:03 +0100

    use host pid from kernel agent

--- native/src/main/native/NativeThread.c
+++ native/src/main/native/NativeThread.c
@@ -1,9 +1,20 @@
 #include <jni.h>
-#include <syscall.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 #include "NativeThread.h"
 
 JNIEXPORT jint JNICALL
 Java_org_jboss_byteman_rule_helper_Helper_currentNativeId(JNIEnv *env, jobject obj) {
-    jint tid = syscall(__NR_gettid);
+    int fd;
+    jint tid;
+
+    fd = open("/dev/kagent", O_RDONLY);
+    if (fd < 0) {
+        return 0;
+    }
+    tid = ioctl(fd, 0);
+
+    close(fd);
     return tid;
 }

afc762bb6e5a8b4236d292beb65c3334d89c58d2
Han Qiao <sweatybridge@gmail.com>
2017-05-17 16:54:25 +0100

    use error number instead of ioctl

--- native/src/main/native/NativeThread.c
+++ native/src/main/native/NativeThread.c
@@ -1,20 +1,18 @@
 #include <jni.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
+#include <errno.h>
 #include "NativeThread.h"
 
 JNIEXPORT jint JNICALL
 Java_org_jboss_byteman_rule_helper_Helper_currentNativeId(JNIEnv *env, jobject obj) {
     int fd;
-    jint tid;
+    jint tid = 0;
 
     fd = open("/var/log/host/kagent", O_RDONLY);
     if (fd < 0) {
-        return 0;
+        tid = errno;
     }
-    tid = ioctl(fd, 0);
 
-    close(fd);
     return tid;
 }

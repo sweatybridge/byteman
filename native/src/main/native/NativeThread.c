1b4d8b4198d8b7da09d177ea5742fee0a13a7e06
Han Qiao <sweatybridge@gmail.com>
2017-05-17 18:33:26 +0100

    work around large error code in sys_open

--- native/src/main/native/NativeThread.c
+++ native/src/main/native/NativeThread.c
@@ -1,18 +1,22 @@
 #include <jni.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
+#include <syscall.h>
 #include "NativeThread.h"
 
 JNIEXPORT jint JNICALL
 Java_org_jboss_byteman_rule_helper_Helper_currentNativeId(JNIEnv *env, jobject obj) {
     int fd;
+    char *buf;
     jint tid = 0;
 
     fd = open("/var/log/host/kagent", O_RDONLY);
     if (fd < 0) {
-        tid = errno;
+        return 0;
     }
 
+    tid = read(fd, buf, 0);
+
+    close(fd);
     return tid;
 }

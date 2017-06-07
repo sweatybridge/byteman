d10ac08fad4e251b772040490aa728736a3358cd
Han Qiao <qh812@imperial.ac.uk>
2017-06-07 03:01:06 +0100

    optimise file descriptor handling

--- native/src/main/native/NativeThread.c
+++ native/src/main/native/NativeThread.c
@@ -4,47 +4,38 @@
 #include <syscall.h>
 #include "NativeThread.h"
 
+int fd;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 JNIEXPORT jint JNICALL
 Java_org_jboss_byteman_rule_helper_Helper_currentNativeId(JNIEnv *env, jclass obj) {
-    int fd;
     char *buf;
     jint tid = 0;
 
-    fd = open("/var/log/host/kagent", O_RDONLY);
+    // should only be called once, otherwise resource leak
+    fd = open("/var/log/host/kagent", O_RDWR);
     if (fd < 0) {
         return 0;
     }
 
     tid = read(fd, buf, 0);
 
-    close(fd);
     return tid;
 }
 
 JNIEXPORT void JNICALL
 Java_org_jboss_byteman_rule_helper_Helper_printTrace(JNIEnv *env, jclass obj, jstring trace) {
-    int fd;
     const char *buf;
     size_t len;
     int rc;
-
-    fd = open("/var/log/host/kagent", O_RDWR);
-    if (fd < 0) {
-        return;
-    }
 
     buf = (*env)->GetStringUTFChars(env, trace, NULL);
     len = (*env)->GetStringUTFLength(env, trace);
 
     rc = write(fd, buf, len);
-    if (rc < 0) {
-        return;
-    }
-
-    close(fd);
+    // fd will be automatically closed on program exit
 }
 #ifdef __cplusplus
 }
